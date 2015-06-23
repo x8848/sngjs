ce87abacb45f308e4a6672a985f91e11ad5eac4e
xx <8848484@gmail.com>
2015-06-24 01:45:07 +0300

    map selected move

--- src/main/resources/knock.js
+++ src/main/resources/knock.js
@@ -1,11 +1,21 @@
 $.getJSON("/sng/domain.json", function (json) {
 
     json.cards.forEach(function (array) {
-        for(var index in array){
-            array[index]={name: array[index], move: ['----']};
-        };
+        for (var index in array) {
+            array[index] = {name: array[index], move: '----'};
+        }
+    });
+
+    json.menu.forEach(function (object) {
+        for (var index in object.values) {
+            object.values[index] = {name: object.values[index], checked: true};
+        }
     });
 
     viewModel = ko.mapping.fromJS(json);
     ko.applyBindings(viewModel);
+
+    viewModel.checkStreet = function () {
+        alert(this.menu); // TODO: doesn't get data from model
+    };
 });
