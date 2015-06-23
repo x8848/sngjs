77db4a37b463f9ddd1a9a5b5e0fe75fb43f76b5b
xx <8848484@gmail.com>
2015-06-23 23:44:50 +0300

    map selected move

--- src/main/resources/knock.js
+++ src/main/resources/knock.js
@@ -1,4 +1,11 @@
 $.getJSON("/sng/domain.json", function (json) {
+
+    json.cards.forEach(function (array) {
+        for(var index in array){
+            array[index]={name: array[index], move: ['----']};
+        };
+    });
+
     viewModel = ko.mapping.fromJS(json);
     ko.applyBindings(viewModel);
 });
