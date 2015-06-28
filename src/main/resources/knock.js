7d7e69081512836ec3796d3e4a4c7fc5674ef518
xx <8848484@gmail.com>
2015-06-29 01:50:38 +0300

    view model change

--- src/main/resources/knock.js
+++ src/main/resources/knock.js
@@ -1,23 +1,37 @@
+var model;
+
 $.getJSON("/sng/domain.json", function (json) {
 
     json.cards.forEach(function (array) {
         for (var index in array) {
-            array[index] = {name: array[index], move: '----'};
+            array[index] = {name: array[index], move: 'Fold'};
         }
     });
 
-    json.menu.forEach(function (object) {
+  /*  json.menu.forEach(function (object) {
         for (var index in object.values) {
             object.values[index] = {name: object.values[index], checked: true};
         }
-    });
+    });*/
+
+    model = json;
 
     viewModel = ko.mapping.fromJS(json);
-    viewModel.checkStreet = function () {
-        (ko.unwrap(this.menu)).forEach(function (object) {
+
+    viewModel.check = function () {
+        alert("hello");
+        /*(ko.unwrap(this.menu)).forEach(function (object) {
             console.log(ko.unwrap(object.name));
-        });
+        });*/
     };
 
     ko.applyBindings(viewModel);
 });
+
+/*
+$(document).ready(function () {
+    $("select").on("change", function () {
+        var color = $(this.selectedOptions).css("background-color");
+        $(this).css("background-color", color);
+    });
+});*/
